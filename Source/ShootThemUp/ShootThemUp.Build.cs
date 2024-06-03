095c02ec8502cd0ca601bd5563240b651e1bcbfe
LightningLAN <LLLightning1@outlook.com>
2024-06-03 09:53:18 +0800

    first commit

---
+++ Source/ShootThemUp/ShootThemUp.Build.cs
@@ -0,0 +1,42 @@
+// Shoot Them Up Game, All Rights Reserved
+
+using UnrealBuildTool;
+
+public class ShootThemUp : ModuleRules
+{
+	public ShootThemUp(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"Niagara" 
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		PublicIncludePaths.AddRange(new string[] 
+		{ 
+			"ShootThemUP/Public/Player", 
+			"ShootThemUP/Public/Components", 
+			"ShootThemUP/Public/Dev", 
+			"ShootThemUP/Public/Weapon", 
+			"ShootThemUP/Public/UI", 
+			"ShootThemUP/Public/Animations", 
+			"ShootThemUP/Public/Pickups",
+      "ShootThemUP/Public/Weapon/Components"
+    });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
